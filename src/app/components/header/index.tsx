2a25a1d947f4a53dabbe3d549115f46512cf07c3
Caio Vellani <caiovellani@outlook.com>
2025-03-11 18:18:41 -0300

    chore: update dependencies and initial app setup
    - Add lucide-react dependency
    - Update and reorder dependencies in package.json
    - Modify pnpm-lock.yaml to reflect dependency changes
    - Update globals.css import syntax
    - Add Header component to layout
    - Update metadata title
    - Add antialiased class to body
    - Minor code formatting changes

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,34 @@
+import { Gamepad2 } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export function Header() {
+  return (
+    <header className="w-full h-28 bg-slate-100 text-black px-2">
+      <div className="max-w-7xl mx-auto flex justify-center items-center h-28 sm:justify-between">
+        <nav className="flex justify-center items-center gap-4">
+          <Link href="/">
+            <Image
+              src="/logo.svg"
+              alt="Logo do site dalygames"
+              width={168}
+              height={39}
+              quality={100}
+              priority
+              className="w-full"
+            />
+          </Link>
+
+          <Link href="/">Games</Link>
+          <Link href="/profile">Perfil</Link>
+        </nav>
+
+        <div className="hidden sm:flex justify-center items-center">
+          <Link href="/profile">
+            <Gamepad2 className="size-9 text-[#475569]" />
+          </Link>
+        </div>
+      </div>
+    </header>
+  )
+}
