3bb700284c446c92e73c7a2a3a7354149ae42604
Caio Vellani <caiovellani@outlook.com>
2025-03-12 18:10:33 -0300

    feat: Add daily game feature and configure image hosting
    - Configure Next.js to allow images from sujeitoprogramador.com
    - Implement daily game fetching from API
    - Add daily game display on home page with link to game details
    - Update Home component to use Container and display game image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,42 @@
-export default function Home() {
+import { Container } from '@/app/components/container'
+import type { GameProps } from '@/utils/types/game'
+import Image from 'next/image'
+import Link from 'next/link'
+
+async function getDayleGame() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_API_UTL}/next-api/?api=game_day`
+    )
+    return res.json()
+  } catch (err) {
+    console.error(err)
+    throw new Error('Failed to fetch data')
+  }
+}
+
+export default async function Home() {
+  const dalyGame: GameProps = await getDayleGame()
+
   return (
-    <main>
-      <div>Hello world!</div>
+    <main className="w-full">
+      <Container>
+        <h1 className="text-center font-bold text-xl mt-8 mb-5">
+          Separamos um jogo exclusivo para você
+        </h1>
+        <Link href={`/game/${dalyGame.id}`}>
+          <section className="w-full bg-black rounded-lg">
+            <Image
+              src={dalyGame.image_url}
+              alt={dalyGame.title}
+              quality={100}
+              priority
+              width={100}
+              height={100}
+            />
+          </section>
+        </Link>
+      </Container>
     </main>
   )
 }
