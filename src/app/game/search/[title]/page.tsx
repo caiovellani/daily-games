42e02db0cad3bed7eda27a161804ab999de4ef3d
Caio Vellani <caiovellani@outlook.com>
2025-03-14 19:12:19 -0300

    feat(search): add game search page with API integration
    - Implemented a search page that fetches game data based on the title.
    - Integrated with the external API using fetch and environment variables.
    - Displayed results using the GameCard component inside a responsive grid.
    - Added basic error handling for API requests.
    - Included an input component for future search functionality

---
+++ src/app/game/search/[title]/page.tsx
@@ -0,0 +1,43 @@
+import { Container } from '@/app/components/container'
+import { GameCard } from '@/app/components/gamecard'
+import { Input } from '@/app/components/input'
+import type { GameProps } from '@/utils/types/game'
+
+async function getData(title: string) {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_API_UTL}/next-api/?api=game&title=${title}`
+    )
+    return res.json()
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export default async function Search({
+  params: { title },
+}: {
+  params: { title: string }
+}) {
+  const games: GameProps[] = await getData(title)
+
+  return (
+    <main className="w-full text-black">
+      <Container>
+        <Input />
+
+        <h1 className="font-bold text-xl mt-8 mb-5">
+          Veja o que encontramos na nossa base:
+        </h1>
+
+        {!games && <p>Esse jogo não foi encontrado...</p>}
+        <section className="grid gap-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {games &&
+            games.map((item) => {
+              return <GameCard key={item.id} data={item} />
+            })}
+        </section>
+      </Container>
+    </main>
+  )
+}
